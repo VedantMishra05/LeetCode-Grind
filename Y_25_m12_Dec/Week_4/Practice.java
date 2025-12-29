3c9c78a800501572b75bb2e1f2913ff53c560a7b
Vedant Mishra <mishrasandhya0364@gmail.com>
2025-12-30 03:36:38 +0530

    Added today's problem

--- Y_25_m12_Dec/Week_4/Practice.java
+++ Y_25_m12_Dec/Week_4/Practice.java
@@ -73,6 +73,26 @@ public class Practice {
         return n > 0 && (n & (n - 1)) == 0;
     }
 
+    // Longest palindrome - return length of longest palindrome
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+        for(char c: s.toCharArray()) {
+            freq[c]++;
+        }
+
+        int length = 0;
+        boolean hasOdd = false;
+
+        for(int count: freq) {
+            if(count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+            }
+        }
+        return hasOdd ? length + 1 : length;
+    }
+
     public static void main(String[] args) {
         
     }
