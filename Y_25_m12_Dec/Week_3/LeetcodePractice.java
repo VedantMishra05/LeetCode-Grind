7636d441aa9bb7e77444de0ccb93db998794cb33
Vedant Mishra <mishrasandhya0364@gmail.com>
2026-01-03 02:36:01 +0530

    Added Jan Week file and did Dec Week 3 description backlog and revision

--- Y_25_m12_Dec/Week_3/LeetcodePractice.java
+++ Y_25_m12_Dec/Week_3/LeetcodePractice.java
@@ -133,6 +133,19 @@ public class LeetcodePractice {
     }
 
     public static boolean binaryOn2D(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length; 
+        int left = 0, right = m *  - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int row = mid / n, col = mid % n;
+            int val = matrix[row][col];
+
+            if(val == target) return true;
+            if(val > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        
         return false;
     }
 
---
+++ Y_26_m1_Jan/Week_1/Practice.java
@@ -0,0 +1,32 @@
+package Y_26_m1_Jan.Week_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice {
+    
+    public int[] plusOne(int[] num) {
+
+        return new int[] {};
+    }
+
+    public int minimumBoxes(int[] apple, int[] capacity) {
+
+        return 0;
+    }
+
+    public int countNegativeInSortedMatrix(int[][] matrix) {
+
+        return 0;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        return new ArrayList<>();
+    }
+
+    public int titleToNumber(String columnNumber) { // excel sheet column number
+
+        return 0;
+    }
+}
